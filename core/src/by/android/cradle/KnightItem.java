31fee997d53174b944a3d25f0ec7eb06c2f88b50
Triputin <3s@bk.ru>
2020-02-21 00:51:04 +0300

    KnightItems added. Sword developed.

---
+++ core/src/by/android/cradle/KnightItem.java
@@ -0,0 +1,128 @@
+package by.android.cradle;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+
+public class KnightItem extends Item {
+
+private KnightItemParams knightItemParams;
+
+    public KnightItem(float x, float y, int width, int height, Stage s, int row, int col, CradleGame cradleGame,KnightItemParams knightItemParams ) {
+        super(x, y, width, height, s, Touchable.disabled, row, col, cradleGame);
+        setSelected(false, null);
+        setBoundaryPolygon(8);
+        this.knightItemParams = knightItemParams;
+        switch(knightItemParams.getKnightItemType()){
+            case Helmet:
+                switch (knightItemParams.getKnightItemSubType()){
+                    case 1:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 2:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 3:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 4:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 5:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                }
+                break;
+            case Armor:
+                switch (knightItemParams.getKnightItemSubType()){
+                    case 1:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 2:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 3:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 4:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 5:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                }
+                break;
+            case Boots:
+                switch (knightItemParams.getKnightItemSubType()){
+                    case 1:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 2:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 3:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 4:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 5:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                }
+                break;
+            case Sword:
+                switch (knightItemParams.getKnightItemSubType()){
+                    case 1:
+                        loadTexture("knightitems/sword01.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 2:
+                        loadTexture("knightitems/sword02.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 3:
+                        loadTexture("knightitems/sword03.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 4:
+                        loadTexture("knightitems/sword01_gold.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 5:
+                        loadTexture("knightitems/sword02_gold.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 6:
+                        loadTexture("knightitems/sword03_gold.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 7:
+                        loadTexture("knightitems/sword01_top.png", (int) getWidth(), (int) getHeight());
+                        break;
+                }
+                break;
+            case Gloves:
+                switch (knightItemParams.getKnightItemSubType()){
+                    case 1:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 2:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 3:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 4:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                    case 5:
+                        loadTexture("gem01pressed.png", (int) getWidth(), (int) getHeight());
+                        break;
+                }
+                break;
+        }
+
+    }
+
+    //Always locked so you can't select it in the field
+    @Override
+    public boolean isLocked(){
+            return true;
+    }
+
+
+}
